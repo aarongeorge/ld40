5a88f5cfd5f9ac53698cc8302608616526512532
Aaron George <aaron.dean.george@gmail.com>
2017-12-05 00:48:56 +1100

    FIXED: Disabling arrow entry

--- src/js/modules/step-sequencer.js
+++ src/js/modules/step-sequencer.js
@@ -75,7 +75,7 @@ const StepSequencer = class {
                 // Check the appropriate key is down
                 if (keyManager.isDown(step.key)) {
 
-                    if (Game.DancerManager.attackingW || Game.DancerManager.attackingA || Game.DancerManager.attackingS || Game.DancerManager.attackingD) {
+                    if (Game.dancerManager.attackingW || Game.dancerManager.attackingA || Game.dancerManager.attackingS || Game.dancerManager.attackingD) {
                         return;
                     }
 
@@ -155,20 +155,20 @@ const StepSequencer = class {
 
         experience.context.fillStyle = '#FF0000';
 
-        if (Game.DancerManager.attackingW) {
-            experience.fillRect(0, 0, 10, experience.size.width);
+        if (Game.dancerManager.attackingW) {
+            experience.context.fillRect(0, 10, experience.size.width, 10);
         }
 
-        if (Game.DancerManager.attackingA) {
-            experience.fillRect(0, 0, 30, experience.size.width);
+        if (Game.dancerManager.attackingA) {
+            experience.context.fillRect(0, 30, experience.size.width, 10);
         }
 
-        if (Game.DancerManager.attackingS) {
-            experience.fillRect(0, 0, 20, experience.size.width);
+        if (Game.dancerManager.attackingS) {
+            experience.context.fillRect(0, 20, experience.size.width, 10);
         }
 
-        if (Game.DancerManager.attackingD) {
-            experience.fillRect(0, 0, 0, experience.size.width);
+        if (Game.dancerManager.attackingD) {
+            experience.context.fillRect(0, 0, experience.size.width, 10);
         }
     }
 };
